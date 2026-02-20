7daac7d14da8fa12934f1d46e82140c6ef8ba395
Iam-Reddy <87537897+Iam-Reddy@users.noreply.github.com>
2026-02-20 17:20:54 -0600

    Update dev.sh
    updated resource group

--- lessons/day26/scripts/dev.sh
+++ lessons/day26/scripts/dev.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 RESOURCE_GROUP_NAME=terraform-state-rg
-STAGE_SA_ACCOUNT=tfstagebackend2024piyush
-DEV_SA_ACCOUNT=tfdevbackend2024piyush
+STAGE_SA_ACCOUNT=tfstagebackend2026shyam
+DEV_SA_ACCOUNT=tfdevbackend2026shyam
 CONTAINER_NAME=tfstate
 
 
 # Create resource group
-az group create --name $RESOURCE_GROUP_NAME --location canadacentral
+az group create --name $RESOURCE_GROUP_NAME --location eastus
 
 # Create storage account for staging environment
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STAGE_SA_ACCOUNT --sku Standard_LRS --encryption-services blob
